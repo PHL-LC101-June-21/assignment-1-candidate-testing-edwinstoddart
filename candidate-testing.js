73fb560dc022f09d50726c4f01f2a8f589bd1c92
Edwin Stoddart <85506195+edwinstoddart@users.noreply.github.com>
2021-06-12 14:46:58 +0000

    Entered the requisite code as well as some superfluous code under [1.2c].

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = '';
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,19 +15,25 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("Hello, Candidate. Please enter your name here: ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (candidateAnswer === "Paul Blart") {
+    console.log("He's a mall cop, and not a woman, " + candidateName + "... That's not the correct answer.");
+  } else if (candidateAnswer === correctAnswer) {
+    console.log("Your answer, my dear candidate " + candidateName + "... was correct!")
+  } else {
+    console.log("Your answer, my dear candidate " + candidateName + "... was incorrect. You have failed, possibly by technicality.")
+  }
 
   let grade;
   
@@ -38,7 +44,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Thank you, " + candidateName + "! We've not much time, so let's get to the questions.")
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
