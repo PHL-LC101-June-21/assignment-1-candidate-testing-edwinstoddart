123e4d0f4c144dd3c8d662afea20c75b8d75dff8
Edwin Stoddart <85506195+edwinstoddart@users.noreply.github.com>
2021-06-18 19:16:33 +0000

    I ended up starting over by reverting to my initial commit to try and reinforce some things in my mind. I'm not sure if you guys see when we revert to previous iterations, but I figure I should explain why I did in case you do.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,46 +3,50 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
-let questions = 1;
-let correctAnswers = 0;
-let candidateAnswers;
+let candidateAnswer = '';
+let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 'What is the minimum crew size for the ISS?'];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
+let candidateAnswers = ['', '', '', '', ''];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Hello, Candidate. Please enter your name here: ")
+  candidateName = input.question('Hello Candidate Q2364, would you please provide your name below:\n')
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  console.log('Please answer the following questions:');
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question((i+1) + ') ' + questions[i] + '\n');
+  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-    console.log("Correct")
-    correctAnswers += 1;
-  } else {
-    console.log("Incorrect")
-  }
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+  console.log(`Thank you for your time. The following will be your answers followed by the correct answers:
+  1) ${candidateAnswers[0]}, ${correctAnswers[0]}
+  2) ${candidateAnswers[1]}, ${correctAnswers[1]}
+  3) ${candidateAnswers[2]}, ${correctAnswers[2]}
+  4) ${candidateAnswers[3]}, ${correctAnswers[3]}
+  5) ${candidateAnswers[4]}, ${correctAnswers[4]}`);
 
-  let grade = (questions / correctAnswers);
+  let grade;
   
+
   return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello " + candidateName + "! We currently only have one question:")
+  console.log(`Candidate Q2364 has been set to ${candidateName}...\nHello ${candidateName}!`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
