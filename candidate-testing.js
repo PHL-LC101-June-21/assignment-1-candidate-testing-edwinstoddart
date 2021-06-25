27c81db490294f1766f68e05544fe7549eb67aa2
Edwin Stoddart <85506195+edwinstoddart@users.noreply.github.com>
2021-06-25 22:52:31 +0000

    Reverted to original completion of part 3 and recreated missing files.

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,50 +1,66 @@
 const input = require('readline-sync');
+
+// TODO 2: modify your quiz app to ask 5 questions //
+
+// TODO 1.1a: Define candidateName // 
 let candidateName = '';
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = '';
-let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ', '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 'What is the minimum crew size for the ISS? '];
+let questions = ['Who was the first American woman in space?', 'True or false: 5 kilometer == 5000 meters?', '(5 + 3)/2 * 10 = ?', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 'What is the minimum crew size for the ISS?'];
 let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
 let candidateAnswers = ['', '', '', '', ''];
 
+
 function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
   candidateName = input.question('Hello Candidate Q2364, would you please provide your name below:\n')
 }
 
 function askQuestion() {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   console.log('Please answer the following questions:');
   for (let i = 0; i < questions.length; i++) {
     candidateAnswers[i] = input.question((i+1) + ') ' + questions[i] + '\n');
   }
+
 }
 
 function gradeQuiz(candidateAnswers) {
-  console.log(`\nThank you for taking the test, ${candidateName}!`)
+
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
   let score = 0;
   for (i = 0; i < candidateAnswers.length; i++) {
-    console.log(`${i+1}) ${questions[i]}`);
-    console.log(`Your Answer: ${candidateAnswers[i]}
-Correct Answer: ${correctAnswers[i]}\n`);
-    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) { score++;
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      score++;
     }
   }
+  /*
+  console.log(`Thank you for your time. The following will be your answers followed by the correct answers:
+  1) ${candidateAnswers[0]}, ${correctAnswers[0]}
+  2) ${candidateAnswers[1]}, ${correctAnswers[1]}
+  3) ${candidateAnswers[2]}, ${correctAnswers[2]}
+  4) ${candidateAnswers[3]}, ${correctAnswers[3]}
+  5) ${candidateAnswers[4]}, ${correctAnswers[4]}`);
+  */
   let grade = (score / correctAnswers.length) * 100;
-  console.log(`Your overall score is: ${grade}`);
-  if (grade >= 80) {
-    console.log('Congratulations! You passed!!')
-  } else {
-    console.log("I'm sorry, but you've failed.")
-  }
+  
+
   return grade;
 }
 
 function runProgram() {
   askForName();
+  // TODO 1.1c: Ask for candidate's name //
   console.log(`Candidate Q2364 has been set to ${candidateName}...\nHello ${candidateName}!`)
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  grade = gradeQuiz(this.candidateAnswers);
+  console.log(`Thank you for taking the test, ${candidateName}! Your score is: ${grade}`)
 }
 
+// Don't write any code below this line //
+// And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
   question: question,
